2225b3fb3467d209e73fc943e459abdf9f3169fe
parsa Honarmand <parsa.Honarmand@gmail.com>
2020-02-12 17:05:37 -0700

    Updates

---
+++ precache-manifest.92271bed6bd951c0aec66548b3779b6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd3ffc1d4d6aab67216799341c16a8f",
+    "url": "/React-calculator/index.html"
+  },
+  {
+    "revision": "9e6a71f7243c9c31d2b2",
+    "url": "/React-calculator/static/css/main.3c259227.chunk.css"
+  },
+  {
+    "revision": "643018cf63eca34cb370",
+    "url": "/React-calculator/static/js/2.2934629c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-calculator/static/js/2.2934629c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e6a71f7243c9c31d2b2",
+    "url": "/React-calculator/static/js/main.bcf61434.chunk.js"
+  },
+  {
+    "revision": "746af6a867f0464dbdfa",
+    "url": "/React-calculator/static/js/runtime-main.83b7ce10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
